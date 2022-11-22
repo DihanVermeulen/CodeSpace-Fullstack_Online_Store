c7628bb15776d25aac84168392e0e0708229118e
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-22 15:30:28 +0200

    Created router

--- Whisker_Exchange-client/src/main.tsx
+++ Whisker_Exchange-client/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Home } from './routes/home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ <RouterProvider router={router}/>
 )
