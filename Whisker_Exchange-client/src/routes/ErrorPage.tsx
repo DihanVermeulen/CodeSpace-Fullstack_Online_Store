9d7fc6a1fffda28b98c86f66cc4133e217093bd2
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-23 06:54:30 +0200

    Updated

--- Whisker_Exchange-client/src/routes/ErrorPage.tsx
+++ Whisker_Exchange-client/src/routes/ErrorPage.tsx
@@ -6,7 +6,7 @@ export default function ErrorPage() {
 
   return (
     <div id="error-page" className="flex h-screen w-screen flex-col items-center justify-center">
-      <h1 className="text-md font-bold text-gray-900">Oops!</h1>
+      <h1 className="text-md font-bold">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
