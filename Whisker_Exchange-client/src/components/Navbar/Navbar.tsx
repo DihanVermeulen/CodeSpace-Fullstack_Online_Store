5ca74a356219c5117ddeffd5377041d17419b655
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-23 10:13:46 +0200

    Added sidenav toggle button

--- Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
+++ Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import logo from '../../assets/logo.png';
+import Sidenav from '../Sidenav/Sidenav';
 
 export const Navbar: React.FC = () => {
     return (
         <nav>
             <img className='logo' src={logo} alt="logo" />
+            <Sidenav />
         </nav>
     )
 }
