8047adb1af84394df679535f2261426c436de1f9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-23 06:51:07 +0200

    Created error page for routing mistakes

---
+++ Whisker_Exchange-client/src/routes/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error:any = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page" className="flex h-screen w-screen flex-col items-center justify-center">
+      <h1 className="text-md font-bold text-gray-900">Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
