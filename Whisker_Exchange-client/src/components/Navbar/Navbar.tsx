b7d59e418d1c10d17093fd142f0009b45e1f42eb
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-23 14:18:20 +0200

    Mapped navbar data as list

--- Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
+++ Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,22 @@
-import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 import Sidenav from '../Sidenav/Sidenav';
+import { NavbarData } from './NavbarData';
 
 export const Navbar: React.FC = () => {
     return (
         <nav className='flex flex-row items-center cursor-pointer'>
             <img className='logo' src={logo} alt="logo" />
+            <ul className='flex flex-row ml-auto mr-5 hidden sm:flex'>
+                {NavbarData.map((item: any, index: any) => (
+                    <li className='list-none'>
+                        <Link className='flex flex-row' to={item.path}>
+                            {item.icon}
+                            <span>{item.title}</span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
             <Sidenav />
         </nav>
     )
