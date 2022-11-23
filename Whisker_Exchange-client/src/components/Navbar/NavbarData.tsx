919d494207af65e40e9a43f9a8492ff5afc56009
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-23 14:17:18 +0200

    Created file for containing navbar data

---
+++ Whisker_Exchange-client/src/components/Navbar/NavbarData.tsx
@@ -0,0 +1,28 @@
+import { HomeIcon, ShoppingBagIcon, ArrowRightOnRectangleIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
+
+export const NavbarData:any = [
+    {
+        title: "Home",
+        path: "/home",
+        icon: <HomeIcon className='h-6 w-6 mx-5'/>,
+        cName: "nav-text"
+    },
+    {
+        title: "Products",
+        path: "/products",
+        icon: <ShoppingBagIcon className='h-6 w-6 mx-5' />,
+        cName: "nav-text"
+    },
+    {
+        title: "Login",
+        path: "/login",
+        icon: <ArrowRightOnRectangleIcon className='h-6 w-6 mx-5' />,
+        cName: "nav-text"
+    },
+    {
+        title: "Logout",
+        path: "#",
+        icon: <ArrowLeftOnRectangleIcon className='h-6 w-6 mx-5' />,
+        cName: "nav-text"
+    }
+]
