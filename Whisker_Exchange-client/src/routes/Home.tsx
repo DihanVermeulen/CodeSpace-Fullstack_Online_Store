bad155d18adf6c21aab97694cffc426c0a0aac71
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-23 09:35:29 +0200

    Added navbar and sidenav

--- Whisker_Exchange-client/src/routes/Home.tsx
+++ Whisker_Exchange-client/src/routes/Home.tsx
@@ -1,7 +1,12 @@
+import { Navbar } from "../components/Navbar/Navbar"
+import Sidenav from "../components/Sidenav/Sidenav"
+
 export const Home: React.FC = () => {
     return (
-        <div>
-            
+        <div className="h-screen w-screen bg-grey-400">
+            <Sidenav />
+            <Navbar />
+            <h1 className="text-3xl font-bold underline">Home</h1>
         </div>
     )
 }
