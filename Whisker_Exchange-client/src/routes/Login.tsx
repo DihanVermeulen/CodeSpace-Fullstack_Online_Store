d3519b20d672c7fc569850de591dd13190002b95
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-24 13:45:04 +0200

    Updated

--- Whisker_Exchange-client/src/routes/Login.tsx
+++ Whisker_Exchange-client/src/routes/Login.tsx
@@ -1,40 +1,39 @@
-import { LockClosedIcon } from "@heroicons/react/24/outline"
+import { LockClosedIcon } from "@heroicons/react/24/outline";
+import { Input } from "@material-tailwind/react";
 
 export const Login: React.FC = () => {
     return (
         <div className="h-screen flex flex-col items-center justify-center text-center">
             <h1 className="text-xl lg:text-2xl font-bold">Sign in to your account</h1>
             <p>Or <a className="text-cyan-300" href='/register'>create an account</a></p>
-            <form className="space-y-6 lg:w-1/3" action="#" method="POST">
+            <form className="lg:w-1/3" method="POST">
                 <input type="hidden" name="remember" defaultValue="true" />
-                <div className="-space-y-px rounded-md shadow-sm">
-                    <div>
+                <div className="rounded-md shadow-sm">
+                    <div className="mb-4">
                         <label htmlFor="email-address" className="sr-only">
                             Email address
                         </label>
-                        <input
-                            id="email-address"
+                        <Input
+                            variant="outlined"
+                            id="email"
                             name="email"
-                            type="email"
-                            autoComplete="email"
+                            type="text"
                             required
-                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Email address"
-                        />
+                            label="Email"
+                            className="sm:text-sm" />
                     </div>
-                    <div>
+                    <div className="mb-10">
                         <label htmlFor="password" className="sr-only">
                             Password
                         </label>
-                        <input
+                        <Input
+                            variant="outlined"
                             id="password"
                             name="password"
                             type="password"
-                            autoComplete="current-password"
                             required
-                            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Password"
-                        />
+                            label="Password"
+                            className="sm:text-sm" />
                     </div>
                 </div>
 
