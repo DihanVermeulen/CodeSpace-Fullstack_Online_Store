f7994b08686c9493a57727f18eff33b519ba5d14
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-24 13:44:53 +0200

    Added transition root to set show

--- Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
+++ Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
@@ -2,22 +2,29 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import Sidenav from '../Sidenav/Sidenav';
 import { NavbarData } from './NavbarData';
+import { Transition } from '@headlessui/react';
+import { useState } from 'react';
 
 export const Navbar: React.FC = () => {
+    const [show, setShow] = useState(true);
+
     return (
-        <nav className='flex flex-row items-center absolute top-0 w-screen'>
-            <img className='logo' src={logo} alt="logo" />
-            <ul className='flex flex-row ml-auto mr-5 hidden sm:flex'>
-                {NavbarData.map((item: any, index: any) => (
-                    <li className='list-none'>
-                        <Link className='flex flex-row cursor-pointer' to={item.path}>
-                            {item.icon}
-                            <span>{item.title}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <Sidenav />
-        </nav>
+        <Transition.Root show={show}>
+
+            <nav className='flex flex-row items-center absolute top-0 w-screen'>
+                <img className='logo' src={logo} alt="logo" />
+                <ul className='flex flex-row ml-auto mr-5 hidden sm:flex'>
+                    {NavbarData.map((item: any, index: any) => (
+                        <li className='list-none'>
+                            <Link className='flex flex-row cursor-pointer' to={item.path}>
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                <Sidenav />
+            </nav>
+        </Transition.Root>
     )
 }
