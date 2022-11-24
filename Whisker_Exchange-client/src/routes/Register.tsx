24c68a6c5b0479f7da5d9d7446a5f1549dc14ad8
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-24 12:52:09 +0200

    Added login html based off of tailwind components

--- Whisker_Exchange-client/src/routes/Register.tsx
+++ Whisker_Exchange-client/src/routes/Register.tsx
@@ -1,7 +1,103 @@
+import { LockClosedIcon } from "@heroicons/react/24/outline";
+
 export const Register: React.FC = () => {
     return (
-        <div>
-            Register
+        <div className="h-screen flex flex-col items-center justify-center text-center">
+            <h1 className="text-xl lg:text-2xl font-bold">Create account</h1>
+            <p>Or <a className="text-cyan-300" href='/login'>Go to login</a></p>
+            <form className="space-y-6 lg:w-1/3" action="#" method="POST">
+                <input type="hidden" name="remember" defaultValue="true" />
+                <div className="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label htmlFor="email-address" className="sr-only">
+                            Name
+                        </label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            autoComplete="name"
+                            required
+                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Name"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="surname" className="sr-only">
+                            Surname
+                        </label>
+                        <input
+                            id="surname"
+                            name="surname"
+                            type="text"
+                            autoComplete="surname"
+                            required
+                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Surname"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="email-address" className="sr-only">
+                            Email address
+                        </label>
+                        <input
+                            id="email-address"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Email address"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="password" className="sr-only">
+                            Password
+                        </label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Password"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col lg:flex-row items-center justify-between">
+                    <div className="flex items-center">
+                        <input
+                            id="remember-me"
+                            name="remember-me"
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                        <label htmlFor="remember-me" className="ml-2 block text-sm text-cyan-300">
+                            Remember me
+                        </label>
+                    </div>
+
+                    <div className="text-sm">
+                        <a href="#" className="font-medium hover:text-indigo-500">
+                            Forgot your password?
+                        </a>
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        className="group relative flex w-full justify-center rounded-md border border-transparent bg-cyan-400 py-2 px-4 text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                        </span>
+                        Sign in
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
