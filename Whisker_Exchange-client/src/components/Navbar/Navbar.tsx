727b2ada489e106e4a4d2154d7a0f98c2b6b0338
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-24 08:59:11 +0200

    Added pointer cursor over li items

--- Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
+++ Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
@@ -5,12 +5,12 @@ import { NavbarData } from './NavbarData';
 
 export const Navbar: React.FC = () => {
     return (
-        <nav className='flex flex-row items-center cursor-pointer'>
+        <nav className='flex flex-row items-center'>
             <img className='logo' src={logo} alt="logo" />
             <ul className='flex flex-row ml-auto mr-5 hidden sm:flex'>
                 {NavbarData.map((item: any, index: any) => (
                     <li className='list-none'>
-                        <Link className='flex flex-row' to={item.path}>
+                        <Link className='flex flex-row cursor-pointer' to={item.path}>
                             {item.icon}
                             <span>{item.title}</span>
                         </Link>
