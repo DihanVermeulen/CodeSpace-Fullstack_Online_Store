55464e79507f48e97dbc40361d62ed5db233edc9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-24 13:50:03 +0200

    Navbar hides on scroll down

--- Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
+++ Whisker_Exchange-client/src/components/Navbar/Navbar.tsx
@@ -3,15 +3,34 @@ import logo from '../../assets/images/logo.png';
 import Sidenav from '../Sidenav/Sidenav';
 import { NavbarData } from './NavbarData';
 import { Transition } from '@headlessui/react';
-import { useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 
 export const Navbar: React.FC = () => {
     const [show, setShow] = useState(true);
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+
+    const handleScroll = () => {
+        const currentScrollPos = window.scrollY
+
+        if (currentScrollPos > prevScrollPos) {
+            setShow(false)
+        } else {
+            setShow(true)
+        }
+
+        setPrevScrollPos(currentScrollPos)
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    })
 
     return (
         <Transition.Root show={show}>
 
-            <nav className='flex flex-row items-center absolute top-0 w-screen'>
+            <nav className='flex flex-row items-center fixed top-0 w-screen'>
                 <img className='logo' src={logo} alt="logo" />
                 <ul className='flex flex-row ml-auto mr-5 hidden sm:flex'>
                     {NavbarData.map((item: any, index: any) => (
