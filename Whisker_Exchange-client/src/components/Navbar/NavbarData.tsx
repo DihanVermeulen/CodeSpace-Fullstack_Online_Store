636a734e41c06f77bd368b01285a27d06cf644df
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-25 14:46:17 +0200

    Updated classes

--- Whisker_Exchange-client/src/components/Navbar/NavbarData.tsx
+++ Whisker_Exchange-client/src/components/Navbar/NavbarData.tsx
@@ -1,29 +1,26 @@
-import { HomeIcon, ShoppingBagIcon, ArrowRightOnRectangleIcon, ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline';
+import { HomeIcon, ShoppingBagIcon, ArrowRightOnRectangleIcon, ArrowLeftOnRectangleIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 export const NavbarData:any = [
     {
         title: "Home",
         path: "/home",
-        icon: <HomeIcon className='h-6 w-6 mx-5'/>,
-        cName: "nav-text"
+        icon: <HomeIcon className='h-6 w-6 mx-3'/>,
     },
     {
         title: "Products",
         path: "/products",
-        icon: <ShoppingBagIcon className='h-6 w-6 mx-5' />,
-        cName: "nav-text"
+        icon: <ShoppingBagIcon className='h-6 w-6 mx-3' />,
     },
     {
         title: "Login",
         path: "/login",
-        icon: <ArrowRightOnRectangleIcon className='h-6 w-6 mx-5' />,
-        cName: "nav-text"
+        icon: <ArrowRightOnRectangleIcon className='h-6 w-6 mx-3' />,
     },
     {
         title: "Logout",
         path: "#",
-        icon: <ArrowLeftOnRectangleIcon className='h-6 w-6 mx-5' />,
-        cName: "nav-text"
+        icon: <ArrowLeftOnRectangleIcon className='h-6 w-6 mx-3' />,
+    },
     {
         title: "Cart",
         path: "/shopping-cart",
