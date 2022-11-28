64170b8c9df8767d8ccf0ecabbae2f5a111f53e4
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-28 11:26:34 +0200

    Created function to make connection to database

--- Whisker_Exchange-server/src/conn/DbConfig.php
+++ Whisker_Exchange-server/src/conn/DbConfig.php
@@ -2,9 +2,27 @@
 class DbConfig
 {
     private $config = array(
-        "user"=> "root",
-        "password"=> "",
-        "db"=> "online_store_db",
-        "host"=> "localhost"
+        "user" => "root",
+        "password" => "",
+        "db" => "online_store_db",
+        "host" => "localhost"
     );
+
+    public function connect()
+    {
+        $mysqli = new mysqli(
+            $this->config['user'],
+            $this->config['password'],
+            $this->config['db'],
+            $this->config['host'],
+        );
+
+        // Check connection
+        if ($mysqli->connect_error) {
+            die("Connection failed: " . $mysqli->connect_error);
+        } else {
+            echo "connected successfully";
+            return $mysqli;
+        }
+    }
 }
